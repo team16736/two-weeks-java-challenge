7e7cdb32e7eec2cb8ab409b9270b21bfcc188421
sperian <sperian@gmrc.com>
2020-03-21 17:38:15 -0500

    intial commit

---
+++ src/main/java/com/atomic/narwhals/challenge/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.atomic.narwhals.challenge;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Thank you for participating in the challenge! ");
+    }
+}
